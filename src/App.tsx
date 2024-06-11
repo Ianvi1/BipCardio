a874a076e01d40d15749da9a8dfdb157e5e67599
Ian Vieira <ianmarques619@gmail.com>
2024-06-11 04:23:11 -0300

    outros testes interessantes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import './globals.css';
 import { BrowserRouter as Router, Routes, Route, } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import CardMedit from "./CardMedit";
+import SobreDisp from "./SobreDisp";
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
         <Route path="/" element={<Start/>} />
         <Route path="/home" element={<Home/>} />
         <Route path="/navbar"  element={<Navbar/>} />
+        <Route path="/cardmedit" element={<CardMedit/>} />
+        <Route path="/sobredisp" element={<SobreDisp/>} />
       </Routes>
       </div>
     </Router>
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,17 +1,26 @@
 import { FaHome, FaReact } from "react-icons/fa";
 import { BsFillPeopleFill } from "react-icons/bs";
 import "./navbar.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  
+  const [activePage, setActivePage] = useState<string>('home');
+
+  const handleSetActivePage = (page: string) => {
+    setActivePage(page);
+  }
+
   return (
       <div className="MobileBar">
           <ul>
-            <li className="Home"><a href="/"><FaHome color="#b5b6b7" size={32}/></a></li>
-            <li className="React"><a href="/"><FaReact color="#b5b6b7" size={32}/></a></li>
-            <li className="People"><a href="/"><BsFillPeopleFill color="#b5b6b7" size={32}/></a></li>
+            <li className={activePage === 'home' ? 'active':''} onClick={() => handleSetActivePage('home')}><Link to="/home" className="navbarHome"><FaHome color={activePage === 'home' ? '#3ce2fb' : '#b5b6b7'} size={32}/></Link></li>
+            <li className={activePage === 'react' ? 'active':''} onClick={() => { setActivePage('react'); }}><Link to="/sobredisp" className="navbarReact"><FaReact color={activePage === 'react' ? '#3ce2fb' : '#b5b6b7'} size={32}/></Link></li>
+            <li className={activePage === 'people' ? 'active':''} onClick={() => handleSetActivePage('people')}><a href="/"><BsFillPeopleFill color={activePage === 'people' ? '#3ce2fb' : '#b5b6b7'} size={32}/></a></li>
           </ul>
       </div>
   )
 }
 
-export default Navbar
+export default Navbar;
